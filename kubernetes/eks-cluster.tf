1d38b6e7d54c929dc2a5fef041873deb14027fa6
Olabode-Dev <aderojuolabode001@gmail.com>
2024-12-16 20:51:43 +0100

    Update eks-cluster.tf

--- kubernetes/eks-cluster.tf
+++ kubernetes/eks-cluster.tf
@@ -1,39 +1,41 @@
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
-  cluster_endpoint_public_access  = true
+  version = "~> 20.31.0"
+
+  cluster_name    = "myAppp-eks-cluster"
+  cluster_version = "1.27"
+  vpc_id          = module.vpc.vpc_id
+  subnet_ids      = module.vpc.private_subnets
+
+  cluster_endpoint_public_access = true
   cluster_addons = {
-    coredns = {
-      most_recent = true
-    }
-    kube-proxy = {
-      most_recent = true
-    }
-    vpc-cni = {
-      most_recent = true
+    coredns = { most_recent = true }
+    kube-proxy = { most_recent = true }
+    vpc-cni = { most_recent = true }
+  }
+
+  iam_role_additional_policies = [
+    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
+    "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  ]
+
+  eks_managed_node_groups = {
+    dev = {
+      min_size       = 1
+      max_size       = 3
+      desired_size   = 2
+      instance_types = ["t2.small"]
+      key_name       = "virginia-kp"
+      iam_role_additional_policies = [
+        "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
+        "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
+        "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+      ]
     }
   }
-  
-
-  cluster_name = "myAppp-eks-cluster"  
-  cluster_version = "1.27"
-
-  subnet_ids = module.myAppp-vpc.private_subnets
-  vpc_id = module.myAppp-vpc.vpc_id
 
   tags = {
     environment = "development"
     application = "myAppp"
-  }
-
-  eks_managed_node_groups = {
-    dev = {
-      min_size     = 1
-      max_size     = 6
-      desired_size = 3
-
-      instance_types = ["t2.small"]
-      key_name       = "virginia kp"
-    }
   }
 }
