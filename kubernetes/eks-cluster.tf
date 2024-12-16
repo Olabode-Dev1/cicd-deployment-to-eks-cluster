4ecb8a372fdeffc0c11cbede3bd75030206d9f4d
Olabode-Dev <aderojuolabode001@gmail.com>
2024-12-16 21:51:01 +0100

    Update eks-cluster.tf

--- kubernetes/eks-cluster.tf
+++ kubernetes/eks-cluster.tf
@@ -1,11 +1,37 @@
+resource "aws_iam_user_policy_attachment" "user_eks_policy" {
+  user       = "your-iam-username"  # Replace with your IAM username
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}
+
+resource "aws_iam_user_policy_attachment" "user_eks_vpc_policy" {
+  user       = "your-iam-username"  # Replace with your IAM username
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+}
+
+resource "kubernetes_config_map" "aws_auth" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+  }
+
+  data = {
+    mapUsers = <<EOF
+- userarn: arn:aws:iam::<account-id>:user/your-iam-username  # Replace with your IAM user ARN
+  username: your-iam-username  # Replace with your IAM username
+  groups:
+    - system:masters
+EOF
+  }
+} 
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.31.0"
 
   cluster_name    = "myAppp-eks-cluster"
   cluster_version = "1.27"
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets
+  subnet_ids      = module.myAppp-vpc.private_subnets
+  vpc_id          = module.myAppp-vpc.vpc_id
 
   cluster_endpoint_public_access = true
   cluster_addons = {
@@ -14,28 +40,26 @@ module "eks" {
     vpc-cni = { most_recent = true }
   }
 
-  iam_role_additional_policies = [
-    "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy",
-    "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  ]
-
   eks_managed_node_groups = {
     dev = {
+      desired_size   = 3
+      max_size       = 6
       min_size       = 1
-      max_size       = 3
-      desired_size   = 2
       instance_types = ["t2.small"]
       key_name       = "virginia-kp"
+      ami_type       = "AL2_x86_64"
+      disk_size      = 20
+
       iam_role_additional_policies = [
         "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly",
         "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy",
         "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
       ]
-    }
-  }
 
-  tags = {
-    environment = "development"
-    application = "myAppp"
+      tags = {
+        environment = "development"
+        application = "myAppp"
+      }
+    }
   }
 }
